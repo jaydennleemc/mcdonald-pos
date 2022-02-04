0e3c3c349a75e9e82f583814ad4c0016c39da91b
jayyyden <jaydennleemc@gmail.com>
2022-02-05 00:32:46 +0800

    Initial Commit

---
+++ mcdonald POS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mcdonald POS
+//
+//  Created by Jayden on 5/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
